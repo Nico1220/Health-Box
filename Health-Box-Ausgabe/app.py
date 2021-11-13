a549bf07fc4ce92732246eecd1105827d4373288
Nico1220 <86952580+Nico1220@users.noreply.github.com>
2021-11-13 10:58:18 +0100

    temperatur test raspberry

--- Health-Box-Ausgabe/app.py
+++ Health-Box-Ausgabe/app.py
@@ -1,28 +1,6 @@
-from flask import Flask, jsonify, request, abort, render_template
-
-app = Flask(__name__)
-
-
-@app.route('/', methods=['GET'])
-def index():
-    return render_template('index.html')
-
-
-@app.route('/api/health-box', methods=['POST'])
-def calculate_bmi():
-    # request enthält die vom Client übergebenen Daten
-    # Parmeter force ...Mimetype ignorieren und immer als JSON parsen
-    data = request.get_json(force=True)
-    try:
-<<<<<<< Updated upstream
-        temp = data['temp']
-        puls = data['puls']
-=======
-        temp = data['temperature']
-        puls = data['pulsoxymeter']
->>>>>>> Stashed changes
-    except:
-        abort(500, "paramerzers needed")
+from tempsens import *
 
 if __name__ == "__main__":
-    app.run()
+    sensor = MLX90614()
+    print("Object:", sensor.readObjectTemperature())
+    print("Ambient:", sensor.readAmbientTemperature())
