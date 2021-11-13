a5cfca7219c04adcb9e901fc10abdf1b0a4a8e09
Nico1220 <86952580+Nico1220@users.noreply.github.com>
2021-11-13 10:24:54 +0100

    parameter update

--- Health-Box-Ausgabe/app.py
+++ Health-Box-Ausgabe/app.py
@@ -14,8 +14,13 @@ def calculate_bmi():
     # Parmeter force ...Mimetype ignorieren und immer als JSON parsen
     data = request.get_json(force=True)
     try:
+<<<<<<< Updated upstream
         temp = data['temp']
         puls = data['puls']
+=======
+        temp = data['temperature']
+        puls = data['pulsoxymeter']
+>>>>>>> Stashed changes
     except:
         abort(500, "paramerzers needed")
 
