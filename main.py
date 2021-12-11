970b14f901527488896a91882ad60993024d00ae
Nico1220 <86952580+Nico1220@users.noreply.github.com>
2021-12-11 11:20:02 +0100

    Zusammensetzung der einzelnen Programme
    Funktionelle python programme UI comming soon

---
+++ main.py
@@ -0,0 +1,31 @@
+from MLX90614 import *
+from heartrate_monitor import HeartRateMonitor
+import time
+import argparse
+
+while(True):
+    if __name__ == "__main__":
+
+        sensor = MLX90614()
+        print("Object:", sensor.readObjectTemperature())
+        print("Ambient:", sensor.readAmbientTemperature())
+        time.sleep(3)
+
+parser = argparse.ArgumentParser(description="Read and print data from MAX30102")
+parser.add_argument("-r", "--raw", action="store_true",
+                    help="print raw data instead of calculation result")
+parser.add_argument("-t", "--time", type=int, default=30,
+                    help="duration in seconds to read from sensor, default 30")
+args = parser.parse_args()
+
+print('sensor starting...')
+hrm = HeartRateMonitor(print_raw=args.raw, print_result=(not args.raw))
+hrm.start_sensor()
+try:
+    time.sleep(args.time)
+except KeyboardInterrupt:
+    print('keyboard interrupt detected, exiting...')
+
+hrm.stop_sensor()
+print('sensor stoped!')
+
