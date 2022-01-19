65ac9269979aa2e6e0edaec97d88f3e79664a3ad
Nico1220 <86952580+Nico1220@users.noreply.github.com>
2022-01-19 13:46:45 +0100

    Finish

---
+++ GUI_ALL.py
@@ -0,0 +1,71 @@
+import os
+import tkinter
+
+import RPi.GPIO as GPIO
+GPIO.setmode(GPIO.BOARD)
+GPIO.setwarnings(False)
+GPIO.setup(18, GPIO.IN)
+
+from smbus import SMBus
+from mlx90614 import MLX90614
+import max30102
+import hrcalc
+
+print("[INFO] MAX30102 & MLX90614 Channel & I2C Address.")
+
+bus = SMBus(1)
+sensor = MLX90614(bus, address=0x5A)
+m = max30102.MAX30102()
+hr2 = 0
+sp2 = 0
+
+
+class App:
+    def __init__(self, window, window_title):
+        self.window = window
+        self.window.title(window_title)
+
+        self.TitleLbl = tkinter.Label(window, text="MAX30102 & MLX90614 WITH RASPBERRY PI",font=("Arial", 20, 'bold'), fg = "black",relief="raised",borderwidth = 2)
+        self.TitleLbl.pack(anchor=tkinter.CENTER, expand=True)
+
+        self.TitleLbl = tkinter.Label(window, text="DEVELOPER : Team-HealthBox",font=("Arial", 20, 'bold'), fg = "dark orchid",relief="raised",borderwidth = 1)
+        self.TitleLbl.pack(anchor=tkinter.CENTER, expand=True)
+        
+        self.PulseLbl = tkinter.Label(window, text="[Heart Pulse Rate    : ]",font=("Arial", 18), fg = "red",relief="ridge",borderwidth = 2)
+        self.PulseLbl.pack(anchor=tkinter.CENTER, expand=True)
+
+        self.SPO2Lbl = tkinter.Label(window, text="[Oxygen Saturation   : ]",font=("Arial", 18), fg ="blue",relief="ridge",borderwidth = 2)
+        self.SPO2Lbl.pack(anchor=tkinter.CENTER, expand=True)
+        
+        self.Temp1Lbl = tkinter.Label(window, text="[Body Temperature    : ]",font=("Arial", 18), fg = "orange",relief="ridge",borderwidth = 2)
+        self.Temp1Lbl.pack(anchor=tkinter.CENTER, expand=True)
+       
+        # After it is called once, the update method will be automatically called every delay milliseconds
+        self.delay = 30
+        self.update()
+
+        self.window.mainloop()
+
+ 
+    def update(self):
+        if(True):
+            red, ir = m.read_sequential()
+            hr,hrb,sp,spb = hrcalc.calc_hr_and_spo2(ir, red)
+            celcius = sensor.get_object_1()+7;
+            ambient = sensor.get_ambient()
+            self.Temp1Lbl['text'] = "[Body Temperature    : "+str(round(celcius, 2))+u"\N{DEGREE SIGN}C]" 
+            if(hrb == True and hr != -999 and hr < 105):
+                hr2 = int(hr)
+                #print("Heart Rate : ",hr2)
+                self.PulseLbl['text'] = "[Heart Pulse Rate    : "+str(hr2)+"bpm]"
+            if(spb == True and sp != -999 and sp < 100):
+                sp2 = int(sp)
+                #print("SPO2       : ",sp2)
+                self.SPO2Lbl['text'] = "[Oxygen Saturation   : "+str(sp2)+"%]"          
+        self.window.after(self.delay, self.update)
+
+
+# Create a window and pass it to the Application object
+root = tkinter.Tk()
+root.geometry("+{}+{}".format(250, 50))
+App(root, "HealthBox")
